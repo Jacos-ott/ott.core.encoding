96cb107b41bf85ba1661707d9da53edd0f70e042
Jacos-ott <jacos@ott-mobile.com>
2025-08-28 09:17:10 +0200

    Add project files.

---
+++ Encoding.cs
@@ -0,0 +1,58 @@
+﻿using System.Security.Cryptography;
+
+namespace ott.core.encoding
+{
+    public class Encoding
+    {
+        public async Task<byte[]> Encrypt(string plainText)
+        {
+            byte[] key = new byte[64];
+            byte[] iv = new byte[16];
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            await swEncrypt.WriteAsync(plainText);
+                        }
+                        return msEncrypt.ToArray();
+                    }
+                }
+            }
+        }
+
+        public async Task<string> Decrypt(byte[] cipherText)
+        {
+            byte[] key = new byte[64];
+            byte[] iv = new byte[16];
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            return await srDecrypt.ReadToEndAsync();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ServiceExt.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ott.core.encoding
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddOTTDataExtension(this IServiceCollection services)
+        {
+            services.AddScoped<Encoding>();
+            return services;
+        }
+    }
+}
